6cda74de7c236271c738cbd1b0878170aa97e25c
Maksim Kornakov <makskornakov@ya.ru>
2023-06-08 09:57:55 +0200

    form types and def obj

--- src/types/form.ts
+++ src/types/form.ts
@@ -1,17 +1,18 @@
-type InputType = 'text' | 'number' | 'incremental' | 'checkbox' | 'radio' | 'select' | 'textarea' | 'date';
+type InputType =
+  | 'text'
+  | 'number'
+  | 'incremental'
+  | 'checkbox'
+  | 'radio'
+  | 'select'
+  | 'textarea'
+  | 'date';
 type SectionType = 'mix' | 'items' | 'reasons' | 'agreements' | 'actions';
 
 interface SimpleInput {
   inputType: InputType;
   value: string | number | boolean;
 }
-
-interface NamedInput extends SimpleInput {
-  name: string;
-}
-
-type MixedSection = NamedInput[];
-
 interface Combination {
   [key: string]: SimpleInput;
 }
@@ -19,11 +20,6 @@ interface Combination {
 type ItemsSection = Combination[];
 
 type ReasonsSection = Combination;
-
-interface AgreementsSection {
-  agreementsMade: string;
-}
-
 interface Action {
   used: boolean;
   performedBy: string;
@@ -36,13 +32,109 @@ interface ActionsSection {
 
 interface FinalFormType {
   customerNumber: SimpleInput;
-  customerDetails: MixedSection;
-  address: MixedSection;
-  internal: MixedSection;
+  customerDetails: Combination;
+  address: Combination;
+  internal: Combination;
   items: ItemsSection;
   reason: ReasonsSection;
-  agreementsMade: AgreementsSection;
+  agreementsMade: Combination;
   actions: ActionsSection;
 }
 
+const actionNames = ['return', 'exchange', 'newReceipt', 'retrieve', 'credit'];
 
+// auto-generate the actions section
+const actionsSection = actionNames.reduce((acc, actionName) => {
+  acc[actionName] = {
+    used: false,
+    performedBy: '',
+    completed: false,
+  };
+  return acc;
+}, {} as ActionsSection);
+
+const reasonNames = ['inputError', 'pickFault', 'customerOrderError', 'damage'];
+
+// auto-generate the reasons section
+const reasonsSection = reasonNames.reduce((acc, reasonName) => {
+  acc[reasonName] = {
+    inputType: 'checkbox',
+    value: false,
+  };
+  return acc;
+}, {} as ReasonsSection);
+
+const defaultForm: FinalFormType = {
+  customerNumber: {
+    inputType: 'number',
+    value: 0,
+  },
+  customerDetails: {
+    businessName: {
+      inputType: 'text',
+      value: '',
+    },
+    contact: {
+      inputType: 'text',
+      value: '',
+    },
+  },
+  address: {
+    street: {
+      inputType: 'text',
+      value: '',
+    },
+    postalCode: {
+      inputType: 'number',
+      value: 0,
+    },
+  },
+  internal: {
+    handlerFov: {
+      inputType: 'text',
+      value: '',
+    },
+    orderNumber: {
+      inputType: 'text',
+      value: '',
+    },
+    formDate: {
+      inputType: 'date',
+      value: '',
+    },
+  },
+  items: [
+    {
+      articleNumber: {
+        inputType: 'number',
+        value: 0,
+      },
+      description: {
+        inputType: 'text',
+        value: '',
+      },
+      unit: {
+        inputType: 'text',
+        value: '',
+      },
+      quantity: {
+        inputType: 'incremental',
+        value: 0,
+      },
+    },
+  ],
+  reason: {
+    textReasons: {
+      inputType: 'textarea',
+      value: '',
+    },
+    ...reasonsSection,
+  },
+  agreementsMade: {
+    text: {
+      inputType: 'textarea',
+      value: '',
+    },
+  },
+  actions: actionsSection,
+};
